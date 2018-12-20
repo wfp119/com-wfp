07bd05dc42f753ca71e561207bc0d525c383755d
wangfeipeng <609145288@qq.com>
2018-12-20 21:37:57 +0800

    Initial commit

---
+++ src/main/java/com/wfp/ComWfpApplication.java
@@ -0,0 +1,14 @@
+package com.wfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComWfpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComWfpApplication.class, args);
+    }
+
+}
+
